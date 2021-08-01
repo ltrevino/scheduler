5712936e9452f747f41e5aa8f1f10427fe9745ba
luis <luis@MacBook-Pro-de-luis.local>
2021-08-01 02:34:47 -0500

    Improvements to look and feel and functionality

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,32 +1,82 @@
-//$('#container').siblings().eq(4).append($('<li>Classmates</li>'));
 
-// var dateHour = moment("00:00:00", "h:mm:ss").format("dddd, MMMM Do YYYY, h:mm:ss a");
- var dateHour = moment("00:00:00", "h:mm:ss");
+//Set initial hour and other variables
+var dateHour = moment("00:00:00", "h:mm:ss");
+var input;
 
-//$("#3a").text(dateHour);
+//Set present day and display in document
+var currentDayEl = $('#currentDay');
+var today = moment().format("dddd, MMMM Do YYYY");
+currentDayEl.text(today);
 
-
-
+//Iterates through hours
 for (let index = 0; index < 24; index++) {
 
-dateHour = dateHour.add(index, "hours");
+    //Sets te template adding id's to specific hours
+    var timeSlot = `<div class="input-group">
+                    <span class="input-group-addon">`+dateHour.format("hh:mm:ss a")+`</span>
+                    <input id="inputTask`+index+`" type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
+                    <span class="input-group-addon"><a href=""><i id="saveTask`+index+`" class="fa fa-clipboard" aria-hidden="true"></i></a></span>
+                    </div>`;
+
+    $('#container').append($(timeSlot));
+
     
 
-var timeSlot = `<div class="input-group">
-<span class="input-group-addon">`+dateHour.format("hh:mm:ss a")+`</span>
-<input id="inputTask" type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
-<span class="input-group-addon"><i class="fa fa-clipboard" aria-hidden="true"></i></span>
-</div>`;
+    //Load saved tasks
+    var savedTask = localStorage.getItem("saveTask"+index);
+    var taskEl = $('#inputTask'+index);
 
-if(dateHour.isBefore("08:00:00", "h:mm:ss") )
-{
-    console.log("funciona");
-    $("#inputTask").prop('disabled', true)
+    //Validates if there is a task schedule to style the slots
+    if(savedTask){
+        taskEl.val(savedTask);
+        taskEl.attr('style','background-color: #F85959;');
+    }
+    else{
+        taskEl.attr('style','background-color: #33FF7A;');
 
+    }
 
+    //Disables non working hours
+    if(dateHour < moment("08:00:00", "h:mm:ss") || dateHour > moment("17:00:00", "h:mm:ss") )
+    {
+    
+        inputBlocked = "#inputTask"+index;
+        $(inputBlocked).prop('disabled', true)
+
+        saveBlocked = "#saveTask"+index;
+        $(saveBlocked).parent().attr("style","pointer-events: none");
+
+        taskEl.attr('style','background-color: #7F7F7F;');
+
+    }
+
+    //Change index
+    dateHour = dateHour.add(1, "hours");
 
 }
 
-$('#container').append($(timeSlot));
 
-}
+//Save to localStorage
+
+var saveEl = $('i');
+
+saveEl.on('click', function () {
+    event.preventDefault();
+
+    //Find the input traversing DOM
+    var selector = $("#"+event.target.id);
+    var taskEl = $(selector).closest('span').siblings('input');
+
+    //Saves in localStorage
+    localStorage.setItem(event.target.id, taskEl.val());
+    
+    //Validates if content was saved to style the slots
+    if(taskEl.val()){
+        taskEl.attr('style','background-color: #F85959;');
+    }
+    else{
+        taskEl.attr('style','background-color: #33FF7A;');
+    }
+
+});
+
---
+++ README.md
@@ -0,0 +1,29 @@
+# An awesome Day Plaanner
+
+## Description
+
+This is a web application that helps you organizing your daily tasks, to be more productive.
+Feel free to use it and plan your daily work.
+
+
+## Rules
+
+```
+For the use of this application, the following instructions should apply.
+
+  - You should keep in mind that in non working hours you could not schedule any task. The text input and the save option are disabled.
+  - During working hours, you will schedule tasks writing directly over the text input and then you can click in the "Save" icon to keep your tasks saved.
+  - The green slot means that no tasks where planned for that time slot.
+  - The red slot means that you already has a planned task.
+  - To delete a task, just remove the description and then click on "Save" icon.
+
+```
+
+Improvements TODO
+
+- Send email notifications.
+- Keep a weekly basis scheduling.
+
+- - -
+© 2021 Luis Alberto Treviño
+You can use this application completely free.
